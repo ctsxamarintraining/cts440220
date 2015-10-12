41c17274585ea36a2693485014a208cac10a5c45
CTS440220 <adilbasha29@gmail.com>
2015-10-12 11:45:29 +0530

    modified

--- Day2Morning/ArrayEx4/ArrayEx4/Program.cs
+++ Day2Morning/ArrayEx4/ArrayEx4/Program.cs
@@ -12,10 +12,22 @@ namespace ArrayEx4
 				new Person{ firstName = "Kiran", lastName = "Kumar", age = 20 },
 				new Person{ firstName = "Shiva", lastName = "Prasad", age = 15 }
 			};
-			MyComparer myComparer = new MyComparer ();
-			Array.Sort (personArray,myComparer);
+			Console.WriteLine ("Sort by age");
+			Array.Sort (personArray,new MyAgeComparer());
 			foreach (Person p in personArray) {
-				Console.WriteLine (p.firstName);
+				Console.WriteLine (p.firstName + " " + p.lastName + "Age : "+p.age);
+			};
+			Console.WriteLine ();
+			Console.WriteLine ("Sort by first name");
+			Array.Sort (personArray,new MyFirstNameComparer());
+			foreach (Person p in personArray) {
+				Console.WriteLine (p.firstName + " " + p.lastName + "Age : "+p.age);
+			};
+			Console.WriteLine ();
+			Console.WriteLine ("Sort by last name");
+			Array.Sort (personArray,new MyLastNameComparer());
+			foreach (Person p in personArray) {
+				Console.WriteLine (p.firstName + " " + p.lastName + "Age : "+p.age);
 			};
 
 		}
@@ -27,11 +39,25 @@ namespace ArrayEx4
 		public int age;
 
 	}
-	class MyComparer : IComparer<Person>
+	class MyAgeComparer : IComparer<Person>
 	{
 		public int Compare(Person e1, Person e2)
 		{
 			return e1.age.CompareTo(e2.age);
+		}
+	}
+	class MyFirstNameComparer : IComparer<Person>
+	{
+		public int Compare(Person e1, Person e2)
+		{
+			return e1.firstName.CompareTo(e2.firstName);
+		}
+	}
+	class MyLastNameComparer : IComparer<Person>
+	{
+		public int Compare(Person e1, Person e2)
+		{
+			return e1.lastName.CompareTo(e2.lastName);
 		}
 	}
 }
