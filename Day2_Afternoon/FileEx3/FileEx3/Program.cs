e982abd99c20637761d419633e538b6635464104
CTS440220 <adilbasha29@gmail.com>
2015-10-12 15:39:57 +0530

    file 3

---
+++ Day2_Afternoon/FileEx3/FileEx3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileEx3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			TextReader input = new StreamReader ("1MB.txt");
+			TextWriter output = new StreamWriter ("1mbDup.txt");
+			char[] buffer = new char[500];
+			int read;
+			while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+			{
+				output.Write (buffer, 0, read);
+			}
+			input.Close ();
+			output.Close ();
+		}
+	}
+}
