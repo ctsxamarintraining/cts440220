742bd98f5eb6008905ef9bf767b0e38a560f5b59
CTS440220 <adilbasha29@gmail.com>
2015-10-12 16:53:58 +0530

    exception sample

---
+++ Day4_Afternoon/Exceptionsample/Exceptionsample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exceptionsample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] intArray = new int[10];
+			int i = 0;
+			do {
+				try{
+					Console.WriteLine("Enter an integer");
+					int ele = Convert.ToInt32(Console.ReadLine());
+					intArray[i++] = ele;
+					int j = 0;
+					string commaSepString = "";
+					foreach (int arrEle in intArray){
+						if(j==0){
+							j++;
+							commaSepString = commaSepString+arrEle;
+						}
+						else
+							commaSepString = commaSepString + "," + arrEle ;
+						
+						
+					}
+					Console.WriteLine("The array elements are : "+commaSepString);
+
+				}
+				catch(IndexOutOfRangeException e){
+					Console.WriteLine(e.Message);
+				}
+				catch(FormatException e){
+					Console.WriteLine(e.Message);
+				}
+				catch(Exception e){
+					Console.WriteLine(e.Message);
+				}
+				finally{
+					Console.WriteLine("finally block");
+				}
+			} while(true);
+		}
+	}
+}
